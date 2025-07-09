42cfa206c443c5fe331676a200f0b61e9daf09f2
hawi-claire <7hawi7@gmail.com>
2025-07-09 14:21:46 +0300

    Add header component

--- client/src/components/shared/DesktopHeader.tsx
+++ client/src/components/shared/DesktopHeader.tsx
@@ -1,11 +1,6 @@
-import {
-  HeartIcon,
-  SearchIcon,
-  ShoppingBagIcon,
-  UserIcon,
-  XIcon,
-} from "lucide-react";
-import logo from "../../../assets/logo/akoma-logo.jpg";
+import { HeartIcon, SearchIcon, ShoppingBagIcon, UserIcon } from "lucide-react";
+import logo from "../../assets/logo/akoma-logo.jpg";
+import { Card, CardContent } from "../../components/ui/card";
 
 const navItems = [
   "Collection",
@@ -13,6 +8,32 @@ const navItems = [
   "Modiweek",
   "Plus Size",
   "Sustainability",
+];
+
+const categories = [
+  "Shop All",
+  "Top & Blouses",
+  "Tees",
+  "Pants",
+  "Jackets & Outwears",
+  "Pullovers",
+  "Dresses & Jumpsuits",
+  "Shorts & Skirts",
+];
+
+const featuredCategories = [
+  {
+    name: "Pants",
+    image: "https://c.animaapp.com/mcuwrrdgAi4Qcu/img/rectangle-12383-1.svg",
+  },
+  {
+    name: "Dresses",
+    image: "https://c.animaapp.com/mcuwrrdgAi4Qcu/img/rectangle-12383-2.svg",
+  },
+  {
+    name: "Blouses",
+    image: "https://c.animaapp.com/mcuwrrdgAi4Qcu/img/rectangle-12383.svg",
+  },
 ];
 
 const Header = () => {
@@ -65,4 +86,63 @@ const Header = () => {
   );
 };
 
-export default Header;
+const SearchPage01 = () => {
+  return (
+    <div className="bg-[#ffffff] flex flex-col items-center w-full min-h-screen">
+      <Header />
+
+      <div className="bg-white w-[1440px] h-[1024px] relative">
+        {/* Background Images */}
+        <img
+          className="absolute w-full h-full top-0 left-0 object-cover"
+          alt="Background"
+          src="https://c.animaapp.com/mcuwrrdgAi4Qcu/img/image-28.png"
+        />
+        <img
+          className="absolute w-full h-full top-0 left-0 object-cover"
+          alt="Overlay"
+          src="https://c.animaapp.com/mcuwrrdgAi4Qcu/img/rectangle-12406.svg"
+        />
+
+        {/* Blurred Foreground Container */}
+        <div className="absolute w-full h-[660px] top-0 left-0 bg-white/60 backdrop-blur-[6.5px]">
+          {/* Category Sidebar */}
+          <div className="absolute top-[142px] left-[108px] flex flex-col gap-6">
+            <h3 className="text-lg font-semibold text-[#0c0c0c]">Category</h3>
+            <div className="flex flex-col gap-2">
+              {categories.map((cat, index) => (
+                <span
+                  key={index}
+                  className="text-sm text-[#404040] hover:text-black cursor-pointer"
+                >
+                  {cat}
+                </span>
+              ))}
+            </div>
+          </div>
+
+          {/* Featured Category Cards */}
+          <div className="absolute top-[142px] left-[660px] flex gap-6">
+            {featuredCategories.map((item, index) => (
+              <Card key={index} className="border-none shadow-none">
+                <CardContent className="p-0">
+                  <img
+                    src={item.image}
+                    alt={item.name}
+                    className="w-52 h-[420px] object-cover"
+                  />
+                  <p className="mt-3 text-center text-sm font-medium text-black">
+                    {item.name}
+                  </p>
+                </CardContent>
+              </Card>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SearchPage01;
+export { Header };
